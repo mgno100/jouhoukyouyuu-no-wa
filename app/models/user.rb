9365a36264d394331a13f5cde2abb44ca5e6bc7f
mgno100 <tls.krl.psk.tnm@gmail.com>
2023-04-25 01:22:56 +0900

    バリデーションを設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  validates :password, format: { with: VALID_PASSWORD_REGEX, message: 'is invalid. Include both letters and numbers' }
+  
+  with_options presence: true do
+    validates :nickname
+    validates :occupation
+    validates :position
+  end
 end
