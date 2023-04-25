e7b7af92b765ddc6c29b611e5be90e3fc7428b24
mgno100 <tls.krl.psk.tnm@gmail.com>
2023-04-25 17:57:10 +0900

    group_usersテーブルの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  has_many :group_users
+  has_many :groups, through: :group_users
 
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
   validates :password, format: { with: VALID_PASSWORD_REGEX, message: 'は半角英数字混合で設定してください' }
