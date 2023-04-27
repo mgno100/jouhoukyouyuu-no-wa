1b560134b4474abc52ac07c66d0a58b3d6ac2b7b
mgno100 <tls.krl.psk.tnm@gmail.com>
2023-04-27 15:14:51 +0900

    テストコードを記述

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Group, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @group = FactoryBot.build(:group)
+  end
+
+  describe 'グループ作成' do
+    context '新規作成できる場合' do
+      it "nameの値が存在すれば作成できる" do
+        expect(@group).to be_valid
+      end
+    end
+    context '新規作成できない場合' do
+      it "nameが空では作成できない" do
+        @group.name = ''
+        @group.valid?
+        expect(@group.errors.full_messages).to include('グループ名を入力してください')
+      end
+    end
+  end
 end
