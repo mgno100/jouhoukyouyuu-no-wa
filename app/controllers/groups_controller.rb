fdc55712e8ed9e472b128515400d6217b1c553a9
mgno100 <tls.krl.psk.tnm@gmail.com>
2023-04-27 15:38:27 +0900

    ログアウト状態の場合は、ログインページへ遷移するように実装

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,4 +1,5 @@
 class GroupsController < ApplicationController
+  before_action :authenticate_user!, only: [:new]
   def index
   end
 
@@ -16,7 +17,7 @@ class GroupsController < ApplicationController
   end
 
   private
-  
+
   def group_params
     params.require(:group).permit(:name, :introduction, user_ids: [])
   end
