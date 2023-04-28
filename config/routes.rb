2c18f00f679b30ad64090c87da081be76aca43bd
mgno100 <tls.krl.psk.tnm@gmail.com>
2023-04-28 22:04:25 +0900

    「参加」をクリックすると、グループに参加できるように記述

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
-  get 'posts/index'
   devise_for :users, controllers: {
     registrations: 'users/registrations',
     sessions: 'users/sessions'
   }
   root to: "top#index"
   resources :groups, only: [:index, :new, :create] do
-    
+    member do
+      get 'join'
+    end
     resources :posts, only: [:index]
   end
 end
