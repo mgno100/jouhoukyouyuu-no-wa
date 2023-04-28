f44f62797196aad16524290d198653094ffc1469
mgno100 <tls.krl.psk.tnm@gmail.com>
2023-04-28 21:49:38 +0900

    グループ参加後に遷移する画面の用意

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+  get 'posts/index'
   devise_for :users, controllers: {
     registrations: 'users/registrations',
     sessions: 'users/sessions'
   }
   root to: "top#index"
-  resources :groups, only: [:index, :new, :create]
+  resources :groups, only: [:index, :new, :create] do
+    
+    resources :posts, only: [:index]
+  end
 end
