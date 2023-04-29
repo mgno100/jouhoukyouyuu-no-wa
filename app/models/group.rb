32d0b2c678d2a7f7d8fd2229c1543cfbdf7490f7
mgno100 <tls.krl.psk.tnm@gmail.com>
2023-04-29 15:07:53 +0900

    作成日時が新しい順に表示されるようにした

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,7 +1,7 @@
 class Group < ApplicationRecord
   has_many :group_users
   has_many :users, through: :group_users
-  has_many :posts
+  has_many :posts, -> { order(created_at: :desc) }
 
   validates :name, presence: true
 end
