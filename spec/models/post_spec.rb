0cdbeb9a0af29492463aa43d6cee10705be96364
mgno100 <tls.krl.psk.tnm@gmail.com>
2023-04-29 17:13:08 +0900

    テストコードを記述

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @post = FactoryBot.build(:post)
+  end
+
+  describe '情報投稿' do
+    context '情報が投稿できるとき' do
+      it 'title、sentenceが存在すれば投稿できる' do
+        expect(@post).to be_valid
+      end
+    end
+    context '情報が投稿できないとき' do
+      it 'titleが空では投稿できない' do
+        @post.title = ''
+        @post.valid?
+        expect(@post.errors.full_messages).to include('タイトル名を入力してください')
+      end
+      it 'sentenceが空では投稿できない' do
+        @post.sentence = ''
+        @post.valid?
+        expect(@post.errors.full_messages).to include('情報の内容文を入力してください')
+      end
+      it 'groupが紐付いていないと投稿できない' do
+        @post.group = nil
+        @post.valid?
+        expect(@post.errors.full_messages).to include('グループを入力してください')
+      end
+      it 'userが紐付いていないと投稿できない' do
+        @post.user = nil
+        @post.valid?
+        expect(@post.errors.full_messages).to include('ユーザーを入力してください')
+      end
+    end
+  end
+
 end
