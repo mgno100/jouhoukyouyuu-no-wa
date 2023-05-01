ca08ba607183a918eb2c97a145d26c86ccd03250
mgno100 <tls.krl.psk.tnm@gmail.com>
2023-05-01 17:10:16 +0900

    コメント投稿機能の実装

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,18 @@
 class CommentsController < ApplicationController
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.new(comment_params)
+    if @comment.save
+      @comment = Comment.new
+      redirect_to group_post_path(@post.group_id, @post)
+    else
+      render template: "posts/show"
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content).merge(user_id: current_user.id, post_id: params[:post_id])
+  end
 end
