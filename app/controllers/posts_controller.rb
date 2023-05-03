ccc75cf2d85635252696a01ddf5de86c810107cf
mgno100 <tls.krl.psk.tnm@gmail.com>
2023-05-04 00:47:18 +0900

    情報編集機能の実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -29,7 +29,15 @@ class PostsController < ApplicationController
 
   def edit
     @post = Post.find(params[:id])
-    @group = Group.find(params[:group_id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to action: :show
+    else
+      render :edit
+    end
   end
 
   def destroy
