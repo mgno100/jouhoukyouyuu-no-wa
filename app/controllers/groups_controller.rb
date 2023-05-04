d0dd9199ff5fa0fbbf52809e51268bf63115592f
mgno100 <tls.krl.psk.tnm@gmail.com>
2023-05-04 16:36:12 +0900

    共通の処理に対して、before_actionを使用するようにした

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,12 +1,12 @@
 class GroupsController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_group, only: [:join, :withdrawal, :show]
 
   def index
     @groups = Group.all.order(created_at: :desc)
   end
 
   def join
-    @group = Group.find(params[:id])
     unless @group.users.exists?(current_user.id)
       @group.users << current_user
       redirect_to group_posts_path(@group.id)
@@ -14,7 +14,6 @@ class GroupsController < ApplicationController
   end
 
   def withdrawal
-    @group = Group.find(params[:id])
     @group.users.delete(current_user)
     redirect_to user_path(current_user.id)
   end
@@ -33,12 +32,15 @@ class GroupsController < ApplicationController
   end
 
   def show
-    @group = Group.find(params[:id])
   end
 
   private
 
   def group_params
     params.require(:group).permit(:name, :introduction, user_ids: [])
+  end
+
+  def set_group
+    @group = Group.find(params[:id])
   end
 end
